20275fbf4bfeee8ed2697985237750f294284ca2
MARIKO\Curio <mariko.haruki.hal@gmail.com>
2026-05-11 01:56:49 +0900

    UniTask、DOTween、UniRxの導入

--- Assets/Script/MainCamera.cs
+++ Assets/Script/MainCamera.cs
@@ -1,16 +1,45 @@
+using UniRx;
 using UnityEngine;
 
-public class MainCamera : MonoBehaviour
+namespace Archaeopteryx
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class MainCamera : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Transform _target;
+        [SerializeField] private Vector3 _offsetPosition;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private readonly ReactiveProperty<Transform> _targetRP = new ReactiveProperty<Transform>();
+
+        private void Awake()
+        {
+            // Inspectorで設定された初期値を流す
+            _targetRP.Value = _target;
+        }
+
+        private void Start()
+        {
+            // ターゲット変更時に追従開始
+            _targetRP
+                .Where(t => t != null)
+                .Select(t => t.ObserveEveryValueChanged(x => x.position))
+                .Switch() // ターゲット変更時に購読切り替え
+                .Subscribe(pos =>
+                {
+                    var current = transform.position;
+                    var target = new Vector3(
+                        pos.x + _offsetPosition.x,
+                        pos.y + _offsetPosition.y,
+                        transform.position.z + _offsetPosition.z);
+
+                    transform.position = Vector3.Lerp(current, target, 0.1f);
+                    Debug.Log(transform.position);
+                }).AddTo(this);
+        }
+
+        // 外部からアタッチ可能
+        public void SetTarget(Transform newTarget)
+        {
+            _targetRP.Value = newTarget;
+        }
     }
 }
